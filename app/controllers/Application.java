668cdc324fdeadb2a97472820c6d2931490decc5
flytosky <Peter.wang1221@gmail.com>
2015-07-17 16:02:59 -0700

    Add Log in and Log out; change logo image

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,19 +4,64 @@ import java.util.Iterator;
 import java.util.Map.Entry;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import play.libs.Json;
 import play.mvc.*;
+import play.data.*;
+import utils.Constants;
+import utils.RESTfulCalls;
 import views.html.*;
 
 public class Application extends Controller {
+	
+	public static Form<Login> loginForm = Form
+			.form(Login.class);
+	
+	public static class Login {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
+	    public String email;
+	    public String password;
+	    
+	    public String validate() {
+	    	ObjectNode jsonData = Json.newObject();
+	    	jsonData.put("email", email);
+	    	jsonData.put("password", password);
+	    	// POST Climate Service JSON data
+	    	JsonNode response = RESTfulCalls.postAPI(Constants.URL_HOST + Constants.CMU_BACKEND_PORT 
+	    				+ Constants.IS_USER_VALID, jsonData);
+	        if (response.get("success") == null) {
+	          return "Invalid user or password";
+	        }
+	        return null;
+	    }
+	    
+	}
+	
+	public static Result home() {
+		return ok(home.render());
+	}
+
+	public static Result login() {
+	    return ok(login.render(loginForm));
+	}
+	
+	public static Result logout() {
+        session().clear();
+        flash("success", "You've been logged out");
+        return redirect(routes.Application.home());
     }
 	
-	public static Result showClimateService() {
-        return ok(serviceNewClimateService.render());
-    }
+	public static Result authenticate() {
+	    loginForm = loginForm.bindFromRequest();
+	    if (loginForm.hasErrors()) {
+	        return badRequest(login.render(loginForm));
+	    } else {
+	        session().clear();
+	        session("email", loginForm.get().email);
+	        return redirect(routes.Application.home());
+	    }
+	}
     
     public static void flashMsg(JsonNode jsonNode){
 		Iterator<Entry<String, JsonNode>> it = jsonNode.fields();
