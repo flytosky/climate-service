c81d36000af54be3cd65a1a020f4752a776c9adb
mingqi830 <mqi@andrew.cmu.edu>
2015-07-20 16:17:33 -0700

    move some ServiceExecutionLog functions in model to controller

--- app/controllers/ServiceExecutionLogController.java
+++ app/controllers/ServiceExecutionLogController.java
@@ -1,32 +1,54 @@
 package controllers;
 
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import models.ServiceExecutionLog;
-import play.Logger;
-import play.data.DynamicForm;
 import play.data.Form;
-import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import utils.Constants;
 import utils.RESTfulCalls;
-import utils.RESTfulCalls.ResponseType;
 import views.html.*;
-import models.*;
 
+import com.fasterxml.jackson.databind.JsonNode;
 
 public class ServiceExecutionLogController extends Controller {
+	
+	private static final String GET_ALL_SERVICE_LOG = Constants.URL_HOST
+			+ Constants.CMU_BACKEND_PORT + Constants.GET_ALL_SERVICE_LOG;
 	
 	final static Form<ServiceExecutionLog> serviceLogForm = Form
 			.form(ServiceExecutionLog.class);
 	
 	public static Result getServiceLog() {
-		return ok(serviceLog.render(ServiceExecutionLog.all(), serviceLogForm));
+		
+		List<ServiceExecutionLog> serviceLogList = new ArrayList<ServiceExecutionLog>();		
+		JsonNode serviceLogNode = RESTfulCalls.getAPI(GET_ALL_SERVICE_LOG);
+		
+		// if no value is returned or error or is not json array
+		if (serviceLogNode == null || serviceLogNode.has("error")
+				|| !serviceLogNode.isArray()) {
+			return ok(serviceLog.render(serviceLogList, serviceLogForm));
+		}
+
+		// parse the json string into object
+		for (int i = 0; i < serviceLogNode.size(); i++) {
+			JsonNode json = serviceLogNode.path(i);
+			ServiceExecutionLog newServiceLog = new ServiceExecutionLog();
+			newServiceLog.setId(json.get("id").asLong());
+			newServiceLog.setServiceId(json.get("climateService").get("id").asLong());
+			newServiceLog.setPurpose(json.get("purpose").asText());
+			newServiceLog.setUserName(json.get("user").get("firstName").asText()
+					+ " " + json.get("user").get("lastName").asText());
+			newServiceLog.setServiceConfigurationId(json
+					.get("serviceConfiguration").get("id").asText());
+			newServiceLog.setExecutionStartTime(json.findPath(
+					"executionStartTime").asText());
+			newServiceLog.setExecutionEndTime(json.findPath("executionEndTime").asText());
+			serviceLogList.add(newServiceLog);
+		}		
+		return ok(serviceLog.render(serviceLogList, serviceLogForm));
 	}
 	
 	public static Result searchServiceLog() {
--- app/models/ServiceExecutionLog.java
+++ app/models/ServiceExecutionLog.java
@@ -1,28 +1,5 @@
 package models;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
-import play.Logger;
-import utils.*;
-
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
-import javax.persistence.CascadeType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
 public class ServiceExecutionLog {
 
 	private long id;
@@ -115,61 +92,14 @@ public class ServiceExecutionLog {
 	public void setDataUrl(String dataUrl) {
 		this.dataUrl = dataUrl;
 	}
-
-	private static final String GET_ALL_SERVICE_LOG = Constants.URL_HOST
-			+ Constants.CMU_BACKEND_PORT + Constants.GET_ALL_SERVICE_LOG;
-
-//	private static final String GET_A_SERVICE_LOG = Constants.URL_HOST
-//			+ Constants.CMU_BACKEND_PORT + Constants.NEW_GET_A_SERVICE_LOG;
-//
-//	private static final String EXECUTION_LOG_QUERY = Constants.URL_HOST
-//			+ Constants.CMU_BACKEND_PORT + Constants.SERVICE_EXECUTION_LOG
-//			+ Constants.SERVICE_EXECUTION_LOG_QUERY;
-
-	/**
-	 * Generate the list of all service log
-	 * 
-	 * @return a list of all the service log
-	 */
-	public static List<ServiceExecutionLog> all() {
-
-		List<ServiceExecutionLog> serviceLog = new ArrayList<ServiceExecutionLog>();
-
-		JsonNode serviceLogNode = RESTfulCalls.getAPI(GET_ALL_SERVICE_LOG);
-
-		// if no value is returned or error or is not json array
-		if (serviceLogNode == null || serviceLogNode.has("error")
-				|| !serviceLogNode.isArray()) {
-			return serviceLog;
-		}
-
-		// parse the json string into object
-		for (int i = 0; i < serviceLogNode.size(); i++) {
-			JsonNode json = serviceLogNode.path(i);
-
-			ServiceExecutionLog newServiceLog = deserializeJsonToServiceLog(json);
-			serviceLog.add(newServiceLog);
-		}
-		return serviceLog;
+	
+	@Override
+	public String toString() {
+		return "ServiceExecutionLog [id=" + id + ", serviceId="
+				+ serviceId + ", userName=" + userName + ", serviceConfigurationId="
+				+ serviceConfigurationId + ", purpose=" + purpose
+				+ ", executionStartTime=" + executionStartTime
+				+ ", executionEndTime=" + executionEndTime + ", plotUrl="
+				+ plotUrl + ", dataUrl=" + dataUrl + "]";
 	}
-
-	private static ServiceExecutionLog deserializeJsonToServiceLog(JsonNode json) {
-		ServiceExecutionLog newServiceLog = new ServiceExecutionLog();
-		newServiceLog.setId(json.get("id").asLong());
-		newServiceLog.setServiceId(json.get("climateService").get("id")
-				.asLong());
-		newServiceLog.setPurpose(json.get("purpose").asText());
-		newServiceLog.setUserName(json.get("user").get("firstName").asText()
-				+ " " + json.get("user").get("lastName").asText());
-		newServiceLog.setServiceConfigurationId(json
-				.get("serviceConfiguration").get("id").asText());
-		newServiceLog.setExecutionStartTime(json.findPath("executionStartTime")
-				.asText());
-		newServiceLog.setExecutionEndTime(json.findPath("executionEndTime")
-				.asText());
-		//newServiceLog.setDatasetLogId(json.findPath("datasetLogId").asText());
-
-		return newServiceLog;
-	}
-
 }
