51840a0ac29c9faacbd807f05a5d24fa8dd410fa
RuoxiaoWang <longma1021@gmail.com>
2015-10-14 21:45:33 -0700

    add get first k datasets function and modify web page

--- app/utils/Constants.java
+++ app/utils/Constants.java
@@ -41,6 +41,7 @@ public class Constants {
 	// dataset
 	public static final String GET_ALL_DATASETS = "/dataset/getAllDatasets/json";
 	public static final String DATASET_QUERY = "/dataset/queryDataset";
+	public static final String GET_MOST_K_POPULAR_DATASETS_CALL = "/dataset/getMostKPopularDatasets";
 	
 	// users
 	public static final String GET_ALL_USERS = "/users/getAllUsers/json";
