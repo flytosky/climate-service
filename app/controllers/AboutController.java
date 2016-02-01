94fa7343ae5d3f503e63ec6e737ba9d0e8f0db38
QiHaoBao <qihao.bao@sv.cmu.edu>
2016-02-01 13:56:29 -0800

    add TAB science use case

--- app/controllers/AboutController.java
+++ app/controllers/AboutController.java
@@ -5,19 +5,23 @@ import views.html.*;
 
 
 public class AboutController extends Controller {
-		
+
     public static Result aboutUs() {
     	return ok(aboutUs.render());
     }
-    
+
+		public static Result scienceUseCase() {
+    	return ok(aboutUs.render());
+    }
+
     public static Result aboutProject(){
     	return ok(aboutProject.render());
     }
-    
+
     public static Result tutorial() {
 		return ok(tutorial.render());
 	}
-    
+
     public static Result FAQs() {
 		return ok(frequentlyAskedQuestions.render());
 	}
